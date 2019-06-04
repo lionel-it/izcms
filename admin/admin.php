f3497ff49230dd4248bf64116b2fb9f13b67477c
Pham Ngoc Tuong <phamlionel1805@outlook.com>
2019-06-04 10:23:47 +0700

    Create admin panel

--- admin/admin.php
+++ admin/admin.php
@@ -1,25 +1,24 @@
-<?php include('../includes/header.php');?>
-<?php include('../includes/mysqli_connect.php');?>
-<?php include('../includes/functions.php');?>
-<?php include('../includes/sidebar-admin.php'); 
-// Kiem tra xem nguoi dung co duoc vao trang admin hay khong?
-        admin_access(); 
-        
+<?php 
+  include_once('../includes/header.php');
 ?>
-<div id="content">
-
-    <h2>Welcome To izCMS Admin Panel</h2>
-    <div>
-        <p>
-           Chào mừng bạn đến với trang quản lý của izCMS. Bạn có thể thêm, xóa và chỉnh sửa bài viết ở đây.
-        </p>
-        
-         <p>
-          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus
-        </p>
-        
-
+    <div id="content-container">
+      <?php 
+        include_once('../includes/sidebar-admin.php');
+      ?>  
+        <div id="content">
+          <h2>  Welcome To izCMS Admin Panel</h2>
+          <div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit maxime quibusdam molestiae aut fugit obcaecati, doloremque minima laudantium suscipit sapiente fuga, deserunt, ipsum magnam repellat soluta doloribus a possimus quasi!.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit maxime quibusdam molestiae aut fugit obcaecati, doloremque minima laudantium suscipit sapiente fuga, deserunt, ipsum magnam repellat soluta doloribus a possimus quasi!.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit maxime quibusdam molestiae aut fugit obcaecati, doloremque minima laudantium suscipit sapiente fuga, deserunt, ipsum magnam repellat soluta doloribus a possimus quasi!.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit maxime quibusdam molestiae aut fugit obcaecati, doloremque minima laudantium suscipit sapiente fuga, deserunt, ipsum magnam repellat soluta doloribus a possimus quasi!.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit maxime quibusdam molestiae aut fugit obcaecati, doloremque minima laudantium suscipit sapiente fuga, deserunt, ipsum magnam repellat soluta doloribus a possimus quasi!.</p>
+          </div>
+        </div>
+      <?php 
+        include_once('../includes/sidebar-b.php');
+      ?>  
     </div>
-
-</div><!--end content-->
-<?php include('../includes/footer.php'); ?>
+<?php 
+  include_once('../includes/footer.php');
+?>

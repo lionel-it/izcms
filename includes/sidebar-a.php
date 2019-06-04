ed071728b40ac222314512a324b4566d6c2a8624
Pham Ngoc Tuong <phamlionel1805@outlook.com>
2019-06-04 10:08:31 +0700

    Cat giao dien

--- includes/sidebar-a.php
+++ includes/sidebar-a.php
@@ -1,47 +1,21 @@
-<div id="content-container">
-    <div id="section-navigation">
-        <h2>Site Navigation</h2>
-	   <ul class="navi">
-       <?php
-       
-        // Xac dinh cat_id de to dam link
-        if(isset($_GET['cid']) && filter_var($_GET['cid'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
-            $cid = $_GET['cid'];
-            $pid = NULL;
-        } elseif(isset($_GET['pid']) && filter_var($_GET['pid'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
-            $pid = $_GET['pid'];
-            $cid = NULL;
-        } else {
-            $cid = NULL;
-            $pid = NULL;
-        }
-        // Cau lenh truy xuat categories
-        $q = "SELECT cat_name, cat_id FROM categories ORDER BY position ASC";
-        $r = mysqli_query($dbc, $q);
-            confirm_query($r, $q);
-        
-        // Lay categories tu csdl
-        while($cats = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-            echo "<li><a href='index.php?cid={$cats['cat_id']}'";
-                if($cats['cat_id'] == $cid) echo "class='selected'"; 
-            echo ">".$cats['cat_name']. "</a>";
-            
-            // Cau lenh truy xuat pages
-            $q1 = "SELECT page_name, page_id FROM pages WHERE cat_id={$cats['cat_id']} ORDER BY position ASC";
-            $r1 = mysqli_query($dbc, $q1);
-                confirm_query($r, $q);
-                echo "<ul class='pages'>";
-            
-            // Lay pages tu csdl
-            while($pages = mysqli_fetch_array($r1, MYSQLI_ASSOC)) { 
-                echo "<li><a href='index.php?pid={$pages['page_id']}'";
-                    if($pages['page_id'] == $pid) echo "class='selected'";
-                echo ">".$pages['page_name']."</a></li>";
-                
-            } // End WHILE pages
-                echo "</ul>";
-            echo "</li>";
-        } // End WHILE cats
-       ?>
-	   </ul>
-</div><!--end section-navigation-->
+<div id="section-navigation">
+    <ul class="navi">
+        <li><a href="#">Home</a>
+        <ul class="pages">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Clients</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+        </li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Clients</a>
+        <ul class="pages">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Clients</a></li>
+        </ul>
+        </li>
+        <li><a href="#">Contact Us</a></li>
+    </ul>
+</div>
--- includes/sidebar-b.php
+++ includes/sidebar-b.php
@@ -1,9 +1,4 @@
 <div id="aside">
-    <div class="box">
-        <h2>Welcome to izwebz</h2>
-    	<p>
-    		Please register to have fully access to our juicies information on the Internet.
-    	</p>
-    </div>
-    <?php include('includes/posts.php'); ?>  	
-</div> <!--end aside-->
+    <h2>Welcome to izwebz</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, ad.</p>
+</div>
